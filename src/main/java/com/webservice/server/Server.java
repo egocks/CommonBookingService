b92e2678af713469b5dad88ee148d9e94eaa9cbb
egocks <egocks@ymail.com>
2018-11-13 16:26:50 +0800

    server URL

--- src/main/java/com/webservice/server/Server.java
+++ src/main/java/com/webservice/server/Server.java
@@ -7,11 +7,11 @@ import com.webservice.service.impl.CommonsBookingServiceImpl;
 public class Server {
 	public static void main(String[] args) throws InterruptedException {
 		CommonsBookingServiceImpl impl = new CommonsBookingServiceImpl();
-		String address = "http://localhost:8080/CommonsBookingService";
+		String address = "http://localhost:8080/CommonsBookingService/example";
 		Endpoint.publish(address, impl);
 		System.out.println("Server ready...");
 		Thread.sleep(99 * 60 * 1000);
-		System.out.println("Server exiting...");
+		System.out.println("Server exciting...");
 		System.exit(0);
 	}
 }
